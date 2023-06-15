8f1ba4a4ec5a9468c6416f58276f84adf07d3f30
sulaiman8 <91103807+sulaiman8@users.noreply.github.com>
2023-06-15 22:54:49 +0700

    Update sidebar.blade.php

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -5,7 +5,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">Homepage</div>
+                <div class="sidebar-brand-text mx-3">Home</div>
             </a>
 
             <!-- Divider -->
@@ -27,13 +27,13 @@
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
-                    <span>Management Product</span>
+                    <span>Manajemen Produk</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="{{ route('admin.categories.index') }}">Category</a>
+                        <a class="collapse-item" href="{{ route('admin.categories.index') }}">Kategori</a>
                         <a class="collapse-item" href="{{ route('admin.tags.index') }}">Tag</a>
-                        <a class="collapse-item" href="{{ route('admin.products.index') }}">Product</a>
+                        <a class="collapse-item" href="{{ route('admin.products.index') }}">Produk</a>
                     </div>
                 </div>
             </li>
