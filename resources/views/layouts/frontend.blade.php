fe35053d7e58f283d4740047f88138dc3851d611
sulaiman8 <sulaiman.alamsyah@mhs.unsoed.ac.id>
2023-06-20 17:13:38 +0700

    update lagi

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -245,13 +245,6 @@
               </ul>
             </div>
           </div>
-          <div class="col-lg-4 col-md-6 col-sm-6 offset-lg-1">
-
-
-          </div>
-          <div class="col-lg-4 col-md-12">
-
-          </div>
         </div>
         <div class="row">
           <div class="col-lg-12">
@@ -259,12 +252,13 @@
               <div class="footer__copyright__text">
                 <p>
                   <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                  
+
                   <script>
                     document.write(new Date().getFullYear());
                   </script>
-                  Dodolan Veggies 
-                  
+                  Dodolan Veggies | This template is made with
+                  <i class="fa fa-heart" aria-hidden="true"></i> by
+                  <a href="https://colorlib.com" target="_blank">Colorlib</a>
                   <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                 </p>
               </div>
