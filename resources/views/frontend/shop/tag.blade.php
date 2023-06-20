ddc16ec309f7cd3ec9c131fd0bdd42122293678c
sulaiman8 <sulaiman.alamsyah@mhs.unsoed.ac.id>
2023-06-20 17:03:37 +0700

    zz

--- resources/views/frontend/shop/tag.blade.php
+++ resources/views/frontend/shop/tag.blade.php
@@ -8,10 +8,10 @@
         <div class="row">
           <div class="col-lg-12 text-center">
             <div class="breadcrumb__text">
-              <h2>Organi Shop</h2>
+              <h2>Dodolan Veggies</h2>
               <div class="breadcrumb__option">
                 <a href="./index.html">Home</a>
-                <span>Shop</span>
+                <span>Belanja</span>
               </div>
             </div>
           </div>
