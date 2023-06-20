4cf1d534047c8677f57b15d670be4cb4136489ec
sulaiman8 <91103807+sulaiman8@users.noreply.github.com>
2023-06-20 20:58:23 +0700

    Update index.blade.php

--- resources/views/frontend/cart/index.blade.php
+++ resources/views/frontend/cart/index.blade.php
@@ -7,10 +7,10 @@
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <div class="breadcrumb__text">
-                        <h2>Shopping Cart</h2>
+                        <h2>Keranjang</h2>
                         <div class="breadcrumb__option">
                             <a href="/">Home</a>
-                            <span>Shopping Cart</span>
+                            <span>Keranjang</span>
                         </div>
                     </div>
                 </div>
@@ -22,7 +22,7 @@
     <!-- Shoping Cart Section Begin -->
     <section class="shoping-cart spad">
         <div class="container" id="cart">
-           
+
         </div>
     </section>
     <!-- Shoping Cart Section End -->
